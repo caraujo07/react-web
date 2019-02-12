0cbfd50c81877c2c81ddb2ceb54fce07744f223a
caraujo07 <caraujo@jc.com.br>
2019-02-12 15:35:13 -0300

    Mostrando o post sem loop infinito

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -22,22 +23,32 @@ class Blog extends Component {
                     author: 'César'
                 }
             })
-            this.setState({posts:updatedPosts});
+            this.setState({ posts:updatedPosts });
             // console.log(response);
         });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
         });
+
         return (
             <div>
                 <section className="Posts">
                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
