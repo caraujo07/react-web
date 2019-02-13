966bc3fedaaeaebdaa9ba76f9f87c5211b71ae2a
caraujo07 <caraujo@jc.com.br>
2019-02-13 15:21:15 -0300

    Instanciando com axios

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-//  axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'; 
-//  axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'; 
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(request => {
